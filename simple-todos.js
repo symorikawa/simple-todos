5a6304674e9c66f694d5bbcc42bcea0974607ae2
Sy Morikawa <sy@hawaii.edu>
2015-10-23 11:50:45 -1000

    Revert "finished step 6 todos tutorial"
    This reverts commit 57dc77391575c27ceb9ec28354f75356ea932e42.

--- simple-todos.js
+++ simple-todos.js
@@ -1,42 +1,23 @@
-Tasks = new Mongo.Collection("tasks");
-
 if (Meteor.isClient) {
-  // This code only runs on the client
-  Template.body.helpers({
-    tasks: function () {
-      // Show newest tasks at the top
-      return Tasks.find({}, {sort: {createdAt: -1}});
+  // counter starts at 0
+  Session.setDefault('counter', 0);
+
+  Template.hello.helpers({
+    counter: function () {
+      return Session.get('counter');
     }
   });
 
-  Template.body.events({
-    "submit .new-task": function (event) {
-      // Prevent default browser form submit
-      event.preventDefault();
-      console.log(event);
-      // Get value from form element
-      var text = event.target.text.value;
-
-      // Insert a task into the collection
-      Tasks.insert({
-        text: text,
-        createdAt: new Date() // current time
-      });
-
-      // Clear form
-      event.target.text.value = "";
+  Template.hello.events({
+    'click button': function () {
+      // increment the counter when button is clicked
+      Session.set('counter', Session.get('counter') + 1);
     }
   });
+}
 
-  Template.task.events({
-    "click .toggle-checked": function () {
-      // Set the checked property to the opposite of its current value
-      Tasks.update(this._id, {
-        $set: {checked: ! this.checked}
-      });
-    },
-    "click .delete": function () {
-      Tasks.remove(this._id);
-    }
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
   });
 }
